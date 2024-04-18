05730d8cb46072b7faa62c0dbe49a1a16a44fb26
Jafet-Gamboa <0322103712@ut-tijuana.edu.mx>
2024-04-17 20:51:31 -0700

    PROYECTO FINAL - ORDENES DE TRABAJO

--- Examen-Parcial2/Examen2/navigation/MainStack.js
+++ Examen-Parcial2/Examen2/navigation/MainStack.js
@@ -4,11 +4,6 @@ import { NavigationContainer, StackActions } from "@react-navigation/native";
 import Home from "../Screens/Home";
 import Uno from "../Screens/Uno";
 import Dos from "../Screens/Dos";
-import Tres from "../Screens/Tres";
-import Cuatro from "../Screens/Cuatro";
-import Cinco from "../Screens/Cinco";
-import Seis from "../Screens/Seis";
-import Siete from "../Screens/Siete";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,14 +11,9 @@ export default function MainStack(){
     return(
        <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Uno" component={Uno} />
-            <Stack.Screen name="Dos" component={Dos} />
-            <Stack.Screen name="Tres" component={Tres} />
-            <Stack.Screen name="Cuatro" component={Cuatro} />
-            <Stack.Screen name="Cinco" component={Cinco} />
-            <Stack.Screen name="Seis" component={Seis} />
-            <Stack.Screen name="Siete" component={Siete} />
+            <Stack.Screen name="Inicio" component={Home} />
+            <Stack.Screen name="Mis Pedidos" component={Pedidos} />
+            <Stack.Screen name="Perfil" component={Perfil} />
         </Stack.Navigator>
        </NavigationContainer>
     );
